705f4e5bfa952a8e8bfda2e0760ffb2d2bbeb1d9
james-mull <152190836+james-mull@users.noreply.github.com>
2026-09-06 21:36:23 -0600

    Fix notification permission callback signature

--- android-app/app/src/main/java/com/astrawave/app/AstraWaveEntryActivity.kt
+++ android-app/app/src/main/java/com/astrawave/app/AstraWaveEntryActivity.kt
@@ -27,7 +27,7 @@ class AstraWaveEntryActivity : ComponentActivity() {
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_NOTIFICATIONS) openMain()
     }
