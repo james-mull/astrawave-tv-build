a0ea4c04eb62161c8241b4db1bbd26c6f9b474b7
james-mull <152190836+james-mull@users.noreply.github.com>
2026-09-13 19:23:51 -0600

    Add web TV catalog directory route

---
+++ web-app/app/app/catalogs/tv/page.tsx
@@ -0,0 +1,5 @@
+import BuiltInCatalogBrowser from '../../BuiltInCatalogBrowser';
+
+export default function TvCatalogsPage(){
+  return <main className="shell"><section className="main" style={{paddingTop:24}}><BuiltInCatalogBrowser kind="series"/></section></main>;
+}
