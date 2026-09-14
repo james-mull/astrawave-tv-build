15230e2037a91df6706ed567af183be2ba80aac6
james-mull <152190836+james-mull@users.noreply.github.com>
2026-09-13 19:23:46 -0600

    Add web Movies catalog directory route

---
+++ web-app/app/app/catalogs/movies/page.tsx
@@ -0,0 +1,5 @@
+import BuiltInCatalogBrowser from '../../BuiltInCatalogBrowser';
+
+export default function MovieCatalogsPage(){
+  return <main className="shell"><section className="main" style={{paddingTop:24}}><BuiltInCatalogBrowser kind="movie"/></section></main>;
+}
